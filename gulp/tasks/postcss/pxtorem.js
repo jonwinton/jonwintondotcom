117cbb3a7d17917e4232b3e132f681b1eaf4fec9
Jon Winton <winton.jon@gmail.com>
2016-04-05 22:24:25 -0400

    Getting baseline CSS setup

--- gulp/tasks/postcss/pxtorem.js
+++ gulp/tasks/postcss/pxtorem.js
@@ -2,7 +2,7 @@ module.exports = {
     rootValue: 16,
     unitPrecision: 5,
     propWhiteList: ['font', 'font-size', 'line-height', 'letter-spacing'],
-    selectorBlackList: [],
+    selectorBlackList: ['body'],
     replace: true,
     mediaQuery: false,
     minPixelValue: 0
