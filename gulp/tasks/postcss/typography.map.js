3ccbd62107f1363af002303a5deff24d77445c16
Jon Winton <winton.jon@gmail.com>
2016-04-05 23:02:12 -0400

    Adding typography scaling

---
+++ gulp/tasks/postcss/typography.map.js
@@ -0,0 +1,52 @@
+module.exports = {
+    typography: {
+        giga: {
+            'font-size': '50px',
+            'line-height': '51px'
+        },
+        mega: {
+            'font-size': '40px',
+            'line-height': '43px'
+        },
+        kilo: {
+            'font-size': '37px',
+            'line-height': '38px'
+        },
+        alpha: {
+            'font-size': '30px',
+            'line-height': '35px'
+        },
+        beta: {
+            'font-size': '28px',
+            'line-height': '32px'
+        },
+        gamma: {
+            'font-size': '24px',
+            'line-height': '32px'
+        },
+        delta: {
+            'font-size': '20px',
+            'line-height': '24px'
+        },
+        epsilon: {
+            'font-size': '18px',
+            'line-height': '32px'
+        },
+        zeta: {
+            'font-size': '16px',
+            'line-height': '24px'
+        },
+        micro: {
+            'font-size': '14px',
+            'line-height': '22px'
+        },
+        milli: {
+            'font-size': '12px',
+            'line-height': '18px'
+        },
+        mini: {
+            'font-size': '10px',
+            'line-height': '18px'
+        }
+    }
+};
