7bf9e10e09ffa6e94e9929d16d204d8c50e7ecbf
Jon Winton <winton.jon@gmail.com>
2016-04-11 19:29:39 -0400

    updating deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+echo 'RUN DEPLOY';
 if [[ $TRAVIS_BRANCH == 'deploy' ]] ; then
-  cd _site;
-  git init;
+    echo 'DEPLOYING!!!!'
+    cd _site
+    touch .nojekyll
+    git init
 
-  git config user.name "Jon Winton";
-  git config user.email "winton.jon@gmail.com";
+    git config user.name "Jon Winton"
+    git config user.email "winton.jon@gmail.com"
 
-  git add .;
-  git commit -m "Deploy";
+    git remote add origin git@github.com:jonwinton/jonwinton.github.io.git
+    git checkout master
+    git add -A
+    git commit -m "Deploying"
 
-  # We redirect any output to
-  # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
+    # We redirect any output to
+    # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+    git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
 else
   echo 'Invalid branch. You can only deploy from deploy.'
   exit 1
