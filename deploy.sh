7d39035495f80308581b4fcedb2cd7e200c3e4be
Jon Winton <winton.jon@gmail.com>
2016-04-11 23:02:33 -0400

    ignoring _site again

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,9 @@
 cd _site;
 touch .nojekyll;
 git init;
-
-git config user.name "Jon Winton"
-git config user.email "winton.jon@gmail.com"
-
-git remote add origin git@github.com:jonwinton/jonwinton.github.io.git;
+git remote add origin https://github.com/jonwinton/jonwinton.github.io.git;
+git fetch;
 git checkout master;
-git/ add -A;
+git add -A;
 git commit -m "Deploying";
 git push origin master;
