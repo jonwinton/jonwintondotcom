1fcb8fd0761fedc2451397c4be98ff18f695e86f
Jon Winton <winton.jon@gmail.com>
2016-04-11 21:56:39 -0400

    Testing

---
+++ _site/README.md
@@ -0,0 +1,12 @@
+# JonWintonDotCom
+
+[![Build Status](https://travis-ci.org/jonwinton/jonwintondotcom.svg?branch=master)](https://travis-ci.org/jonwinton/jonwintondotcom)
+
+A personal website.
+
+## Built With
+
+- Jekyll
+- Gulp
+- PostCSS
+- Webpack (Future)
--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,22 @@
 
 echo 'RUN DEPLOY';
 # if [[ $TRAVIS_BRANCH == 'deploy' ]] ; then
-echo 'DEPLOYING!!!!'
-cd _site
-touch .nojekyll
-git init
+echo 'DEPLOYING!!!!';
+cd _site;
+touch .nojekyll;
+# git init;
 
-git config user.name "Jon Winton"
-git config user.email "winton.jon@gmail.com"
+# git config user.name "Jon Winton"
+# git config user.email "winton.jon@gmail.com"
 
-git remote add origin git@github.com:jonwinton/jonwinton.github.io.git
-git checkout master
-git add -A
-git commit -m "Deploying"
+# git remote add origin git@github.com:jonwinton/jonwinton.github.io.git;
+# git checkout master;
+# git add -A;
+# git commit -m "Deploying";
 
 # We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
+# git push origin master;
 # else
   # echo 'Invalid branch. You can only deploy from deploy.'
   # exit 1
