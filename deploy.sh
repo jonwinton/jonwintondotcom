47c19e651082ee268a3ebb00bb1a04bc779d9efa
Jon Winton <winton.jon@gmail.com>
2016-04-11 23:21:27 -0400

    Another build script update

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ touch .nojekyll;
 git init;
 git remote add origin https://github.com/jonwinton/jonwinton.github.io.git;
 git fetch;
+git push origin --delete master;
 git checkout master;
 git add -A;
 git commit -m "Deploying";
