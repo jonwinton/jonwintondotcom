e671d9935c6d6929cd396a496de591429f139c53
Jon Winton <winton.jon@gmail.com>
2016-04-11 22:25:57 -0400

    Further testing

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-echo 'RUN DEPLOY';
-# if [[ $TRAVIS_BRANCH == 'deploy' ]] ; then
-echo 'DEPLOYING!!!!';
 cd _site;
 touch .nojekyll;
-# git init;
+git init;
 
-# git config user.name "Jon Winton"
-# git config user.email "winton.jon@gmail.com"
+git config user.name "Jon Winton"
+git config user.email "winton.jon@gmail.com"
 
-# git remote add origin git@github.com:jonwinton/jonwinton.github.io.git;
-# git checkout master;
-# git add -A;
-# git commit -m "Deploying";
-
-# We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-# git push origin master;
-# else
-  # echo 'Invalid branch. You can only deploy from deploy.'
-  # exit 1
-# fi
+git remote add origin git@github.com:jonwinton/jonwinton.github.io.git;
+git checkout master;
+git/ add -A;
+git commit -m "Deploying";
+git push origin master;
